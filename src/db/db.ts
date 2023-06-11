fa71ede552ffbbc9dd59d8b87a4fb66259153513
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-11 20:57:40 +0600

    database connected using db

---
+++ src/db/db.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+import pg from "pg";
+dotenv.config();
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to Neon PostgreSQL database");
+  }
+});
+export default pool;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import cors from "cors";
 import * as dotenv from "dotenv";
 import express from "express";
+import pool from "./db/db";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.get("/", (req, res) => {
+  const db = pool.query(`SELECT * FROM users`);
+  res.send("Hello World");
+});
 
 app.use(cors());
 app.use(express.json());
