fc5007790abdf5d71e24ef90059ae6ad23457976
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-12 13:06:00 +0600

    migration created

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import cors from "cors";
 import * as dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
 import { db } from "./db/drizzle";
+import { users } from "./schema/schema";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   try {
@@ -17,8 +20,20 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.use(cors());
-app.use(express.json());
+app.post("/", async (req: Request, res: Response) => {
+  const { fullName, email } = req.body;
+  console.log(req.body);
+  try {
+    const result = await db
+      .insert(users)
+      .values({ fullName: fullName, email: email })
+      .returning();
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.listen(port, () => {
   console.log(`App is running on port ${port}!`);
 });
